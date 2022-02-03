073fa47504ffdbc147b767ca7b6aa8e23d43f786
TMR <chetandagajipatil333@gmail.com>
2022-02-04 00:52:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/firebaselogs/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.firebaselogs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        val topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        val bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+        val logo = findViewById<ImageView>(R.id.imageView)
+        val textLogo = findViewById<TextView>(R.id.textView)
+        val slogan = findViewById<TextView>(R.id.textView2)
+        logo.startAnimation(topAnim)
+        textLogo.startAnimation(bottomAnim)
+        slogan.startAnimation(bottomAnim)
+    }
+}
